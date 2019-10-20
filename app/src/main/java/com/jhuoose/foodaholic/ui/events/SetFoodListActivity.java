76e5b3634a9bd0f0d224c67b0777e62b5efbc148
Yue Chen <993103622@qq.com>
2019-10-20 05:23:47 -0400

    Complete the function of deleting food and Fix bugs of adding Food List. Include: You cannot add the same food. You cannot add nothing to the food list

--- app/src/main/java/com/jhuoose/foodaholic/ui/events/SetFoodListActivity.java
+++ app/src/main/java/com/jhuoose/foodaholic/ui/events/SetFoodListActivity.java
@@ -1,11 +1,14 @@
 package com.jhuoose.foodaholic.ui.events;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.jhuoose.foodaholic.R;
 import com.jhuoose.foodaholic.model.Food;
@@ -42,21 +45,53 @@ public class SetFoodListActivity extends AppCompatActivity {
         addFoodBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Todo: U should check the input food name before add it.
-                //Todo: If is null, notify user and do not add it.
-                //Todo: Same food check.
-
                 foodName = foodNameEt.getText().toString();
+                if (foodName==null||foodName.equals("")){
+                    Toast.makeText(SetFoodListActivity.this,"Invalid foodName",Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    for (int num=0;num<foodList.size();num++){
+                        if (foodList.get(num).getFoodName().equals(foodName)){
+                            Toast.makeText(SetFoodListActivity.this,"Do not add the same food",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                }
+
                 final Button newFood = new Button(SetFoodListActivity.this);
                 newFood.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
                 newFood.setText(foodName);
                 newFood.setId(foodList.size()+1);
 
-                //Todo: If touch the added food, you can edit it or delete it.
                 newFood.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(SetFoodListActivity.this);
+                        builder.setTitle("Delete "+foodName+" ?");
+                        builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                deleteFoodDialog.dismiss();
+                            }
+                        });
 
+                        builder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                for (int j=0;j<foodList.size();j++){
+                                    if (foodList.get(j).getFoodName().equals(foodName)){
+                                        foodList.remove(j);
+                                        break;
+                                    }
+                                }
+                                foodListLayout.removeView(newFood);
+                                deleteFoodDialog.dismiss();
+                                Toast.makeText(SetFoodListActivity.this, foodName+" is Deleted",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        deleteFoodDialog = builder.create();
+                        deleteFoodDialog.show();
                     }
                 });
 
