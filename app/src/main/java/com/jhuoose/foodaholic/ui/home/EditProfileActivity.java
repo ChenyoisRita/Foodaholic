dde231059c0530c58403a85ec486d1758de3a0d3
Shuofeng Wang <shuofengwang666@gmail.com>
2019-11-23 23:32:25 -0500

    Complete Update Profile Function.
    Todo: Update Profile Image;
    
    Bug: Cannot show the updated profile in Home Fragment.

---
+++ app/src/main/java/com/jhuoose/foodaholic/ui/home/EditProfileActivity.java
@@ -0,0 +1,103 @@
+package com.jhuoose.foodaholic.ui.home;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.jhuoose.foodaholic.R;
+import com.jhuoose.foodaholic.api.HerokuAPI;
+import com.jhuoose.foodaholic.api.HerokuService;
+import com.jhuoose.foodaholic.viewmodel.UserProfile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EditProfileActivity extends AppCompatActivity {
+    private HerokuAPI heroku;
+    ImageView profilePic;
+    EditText userName, userPhone;
+    Button confirmBtn,cancelBtn;
+    UserProfile profile = new UserProfile();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        heroku = HerokuService.getAPI();
+        profilePic = findViewById(R.id.editProfilePic_im);
+        userName = findViewById(R.id.userName_et);
+        userPhone = findViewById(R.id.userPhone_et);
+        confirmBtn = findViewById(R.id.editProfile_OK_btn);
+        cancelBtn = findViewById(R.id.editProfile_cancel_btn);
+
+        initializeFields();
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (canCreateProfile()) {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put(String.valueOf(profile.getId()), profile);
+                    Call<UserProfile> call = heroku.updateCurrentUserProfile(map);
+                    call.enqueue(new Callback<UserProfile>() {
+                        @Override
+                        public void onResponse(Call<UserProfile> call, Response<UserProfile> response) {
+                            if (response.isSuccessful()) {
+                                Toast.makeText(EditProfileActivity.this, "Update Profile Successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(EditProfileActivity.this, "Response Error: "+response.code(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<UserProfile> call, Throwable t) {
+                            Toast.makeText(EditProfileActivity.this, "Connection Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void initializeFields() {
+//      Todo: update User profile image
+        profile = HomeFragment.currentUserProfile;
+        userName.setText(profile.getUserName());
+        userPhone.setText(profile.getPhone());
+    }
+
+    public boolean canCreateProfile() {
+        if (userName.getText().equals("") || userName.getText()==null) {
+            Toast.makeText(EditProfileActivity.this, "Invalid Name", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            profile.setUserName(userName.getText().toString().trim());
+        }
+
+        if (userPhone.getText().equals("") || userPhone.getText()==null) {
+            Toast.makeText( EditProfileActivity.this, "Invalid Phone Type", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            profile.setPhone(userPhone.getText().toString().trim());
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/com/jhuoose/foodaholic/ui/home/HomeFragment.java
+++ app/src/main/java/com/jhuoose/foodaholic/ui/home/HomeFragment.java
@@ -2,30 +2,35 @@ package com.jhuoose.foodaholic.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jhuoose.foodaholic.R;
+import com.jhuoose.foodaholic.api.HerokuAPI;
+import com.jhuoose.foodaholic.api.HerokuService;
+import com.jhuoose.foodaholic.ui.LoginActivity;
+import com.jhuoose.foodaholic.viewmodel.UserProfile;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-
-import com.jhuoose.foodaholic.R;
-import com.jhuoose.foodaholic.ui.LoginActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
+    public static UserProfile currentUserProfile;
     Button ChangePassword, logoutButton;
     ImageView profilePicture, editBioImage;
     TextView profileName;
-    EditText profileBio;
+    HerokuAPI herokuAPI;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -37,15 +42,47 @@ public class HomeFragment extends Fragment {
 //        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        herokuAPI = HerokuService.getAPI();
         AlphaAnimation fadein = new AlphaAnimation(0.0f,1.0f);
         ChangePassword = root.findViewById(R.id.btn_changepw);
         logoutButton = root.findViewById(R.id.btn_logout);
         profilePicture = root.findViewById(R.id.profilePic_im);
         editBioImage = root.findViewById(R.id.Edit_imageView);
         profileName = root.findViewById(R.id.profileName_tv);
-        profileBio = root.findViewById(R.id.bio_textView);
 
 
+        Call<UserProfile> call = herokuAPI.getCurrentUserProfile();
+        Log.i("HomeLog", "Seem get User Profile");
+        call.enqueue(new Callback<UserProfile>() {
+
+            @Override
+            public void onResponse(Call<UserProfile> call, Response<UserProfile> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Response Successful", Toast.LENGTH_LONG).show();
+                    profileName.setText(response.body().getUserName());
+                    setCurrentUserProfile(response.body());
+                    Log.i("HomeLog", "Get Successful");
+                }else{
+                    Toast.makeText(getContext(), "Response Error: "+response.code(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserProfile> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(getContext(), "Connection Failed", Toast.LENGTH_LONG).show();
+                Log.i("HomeLog", "Get Fail");
+            }
+        });
+
+        editBioImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), EditProfileActivity.class));
+            }
+        });
+
+        Log.i("HomeLog", "Call back over");
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,13 +93,14 @@ public class HomeFragment extends Fragment {
             }
         });
 
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
+    }
+
+    public UserProfile getCurrentUserProfile() {
+        return currentUserProfile;
+    }
+
+    public void setCurrentUserProfile(UserProfile currentUserProfile) {
+        this.currentUserProfile = currentUserProfile;
     }
 }
